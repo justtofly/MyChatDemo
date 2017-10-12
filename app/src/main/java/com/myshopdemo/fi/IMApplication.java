1e4fe3776585c11c99986a2c9fc25cb8d8ddd001
FlyStorm <ggloading@gmail.com>
2017-10-13 00:10:57 +0800

    easeui的初始化

---
+++ app/src/main/java/com/myshopdemo/fi/IMApplication.java
@@ -0,0 +1,32 @@
+package com.myshopdemo.fi;
+
+import android.app.Application;
+
+import com.hyphenate.chat.EMOptions;
+import com.hyphenate.easeui.controller.EaseUI;
+
+/**
+ * 创建者     yangyanfei
+ * 创建时间   2017/10/12 0012 22:29
+ * 作用	      ${TODO}
+ * <p/>
+ * 版本       $$Rev$$
+ * 更新者     $$Author$$
+ * 更新时间   $$Date$$
+ * 更新描述   ${TODO}
+ */
+//类名是灰色的，说明还没有配置
+public class IMApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //初始化EaseUI
+        EMOptions options = new EMOptions();
+        options.setAcceptInvitationAlways(false);//设置同意后才能接受邀请
+        options.setAutoAcceptGroupInvitation(false);//设置需要同意后才能接受邀请
+
+//        EaseUI.getInstance().init(this, options);
+        EaseUI.getInstance().init(this,options);
+    }
+}
--- app/src/main/java/com/myshopdemo/fi/controller/activity/MainActivity.java
+++ app/src/main/java/com/myshopdemo/fi/controller/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.myshopdemo.fi;
+package com.myshopdemo.fi.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.myshopdemo.fi.R;
 
 public class MainActivity extends AppCompatActivity {
 
