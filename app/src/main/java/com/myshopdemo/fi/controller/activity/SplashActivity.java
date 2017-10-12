9534e7dadf2888e02347de74619dd6cb0a579461
FlyStorm <ggloading@gmail.com>
2017-10-13 00:33:55 +0800

    欢迎页面的实现，延时2秒跳转到主页面或者登录页面

---
+++ app/src/main/java/com/myshopdemo/fi/controller/activity/SplashActivity.java
@@ -0,0 +1,63 @@
+package com.myshopdemo.fi.controller.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import com.hyphenate.chat.EMClient;
+import com.myshopdemo.fi.R;
+
+public class SplashActivity extends Activity {
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            //如果当前activity已经退出，那么就不处理handler中的消息
+            if (isFinishing()) {
+                return;
+            }
+            //判断进入主页面还是登录页面
+            toMainOrLogin();
+        }
+
+    };
+
+    private void toMainOrLogin() {
+        new Thread() {
+            @Override
+            public void run() {
+                //判断当前帐号是否已经登录过
+                if (EMClient.getInstance().isLoggedInBefore()) {//登录过
+                    //获取当前用户的登录信息
+
+                    //跳转到主页面
+                    Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }else {//没登录过
+                    //跳转到主页面
+                    Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //发送2s延时消息
+        mHandler.sendMessageDelayed(Message.obtain(), 2000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //销毁消息
+        mHandler.removeCallbacksAndMessages(null);
+    }
+}
