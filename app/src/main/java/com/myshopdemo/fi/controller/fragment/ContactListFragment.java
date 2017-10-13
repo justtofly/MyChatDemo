5ce30efe138244c86688dbcd4e4fd388aa59eef4
FlyStorm <ggloading@gmail.com>
2017-10-13 21:57:10 +0800

    联系人列表页面的布局

--- app/src/main/java/com/myshopdemo/fi/controller/fragment/ContactListFragment.java
+++ app/src/main/java/com/myshopdemo/fi/controller/fragment/ContactListFragment.java
@@ -1,29 +1,30 @@
 package com.myshopdemo.fi.controller.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.hyphenate.easeui.ui.EaseContactListFragment;
+import com.myshopdemo.fi.R;
 
 /**
  * 创建者     yangyanfei
  * 创建时间   2017/10/13 0013 15:53
- * 作用	      ${TODO}
+ * 作用	      联系人列表页面
  * <p/>
  * 版本       $$Rev$$
  * 更新者     $$Author$$
  * 更新时间   $$Date$$
  * 更新描述   ${TODO}
  */
-public class ContactListFragment extends Fragment {
-    @Nullable
+public class ContactListFragment extends EaseContactListFragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        TextView textView=new TextView(getActivity());
-        textView.setText("联系人页面Fragment");
-        return textView;
+    protected void initView() {
+        super.initView();
+
+        //布局显示加号
+        titleBar.setRightImageResource(R.drawable.em_add);
+
+        //添加头布局
+        View headerView=View.inflate(getActivity(),R.layout.header_fragment_contact,null);
+        listView.addHeaderView(headerView);
     }
 }
