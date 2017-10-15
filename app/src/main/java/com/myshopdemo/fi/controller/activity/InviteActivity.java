bd892e4dcbf7feefc6de6d276b4a845357b44e51
FlyStorm <ggloading@gmail.com>
2017-10-15 10:10:49 +0800

    红点的处理,好友邀请条目的监听

---
+++ app/src/main/java/com/myshopdemo/fi/controller/activity/InviteActivity.java
@@ -0,0 +1,15 @@
+package com.myshopdemo.fi.controller.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.myshopdemo.fi.R;
+
+public class InviteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite);
+    }
+}
--- app/src/main/java/com/myshopdemo/fi/controller/fragment/ContactListFragment.java
+++ app/src/main/java/com/myshopdemo/fi/controller/fragment/ContactListFragment.java
@@ -1,11 +1,20 @@
 package com.myshopdemo.fi.controller.fragment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.hyphenate.easeui.ui.EaseContactListFragment;
 import com.myshopdemo.fi.R;
 import com.myshopdemo.fi.controller.activity.AddContactActivity;
+import com.myshopdemo.fi.controller.activity.InviteActivity;
+import com.myshopdemo.fi.utils.Constant;
+import com.myshopdemo.fi.utils.SpUtils;
 
 /**
  * 创建者     yangyanfei
@@ -18,6 +27,17 @@ import com.myshopdemo.fi.controller.activity.AddContactActivity;
  * 更新描述   ${TODO}
  */
 public class ContactListFragment extends EaseContactListFragment {
+    private ImageView             mIv_contact_red;
+    private LocalBroadcastManager mLocalBroadcastManager;
+    private BroadcastReceiver ContactInviteChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //更新红点显示
+            mIv_contact_red.setVisibility(View.VISIBLE);
+            SpUtils.getInstance().save(SpUtils.IS_NEW_INVITE,true);
+        }
+    };
+
     @Override
     protected void initView() {
         super.initView();
@@ -26,8 +46,28 @@ public class ContactListFragment extends EaseContactListFragment {
         titleBar.setRightImageResource(R.drawable.em_add);
 
         //添加头布局
-        View headerView=View.inflate(getActivity(),R.layout.header_fragment_contact,null);
+        View headerView = View.inflate(getActivity(), R.layout.header_fragment_contact, null);
         listView.addHeaderView(headerView);
+
+        //获取红点对象
+        mIv_contact_red = (ImageView) headerView.findViewById(R.id.iv_contact_red);
+
+        //获取邀请信息条目的对象
+        LinearLayout ll_contact_invite= (LinearLayout) headerView.findViewById(R.id.ll_contact_invite);
+
+        //邀请信息条目点击事件
+        ll_contact_invite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //红点处理
+                mIv_contact_red.setVisibility(View.GONE);
+                SpUtils.getInstance().save(SpUtils.IS_NEW_INVITE,false);
+
+                //跳转到邀请信息列表页面
+                Intent intent=new Intent(getActivity(),InviteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -39,9 +79,25 @@ public class ContactListFragment extends EaseContactListFragment {
             @Override
             public void onClick(View v) {
                 //跳转到添加联系人页面
-                Intent intent=new Intent(getActivity(),AddContactActivity.class);
+                Intent intent = new Intent(getActivity(), AddContactActivity.class);
                 startActivity(intent);
             }
         });
+
+        //初始化红点显示
+        boolean isNewInvite= SpUtils.getInstance().getBoolean(SpUtils.IS_NEW_INVITE,false);
+        mIv_contact_red.setVisibility(isNewInvite?View.VISIBLE:View.GONE);
+
+        //注册广播
+        mLocalBroadcastManager = LocalBroadcastManager.getInstance(getActivity());
+        mLocalBroadcastManager.registerReceiver(ContactInviteChangeReceiver,new IntentFilter(Constant.CONTACT_INVITE_CHENGED));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        //关闭广播（注册了广播就一定要关闭广播）
+        mLocalBroadcastManager.unregisterReceiver(ContactInviteChangeReceiver);
     }
 }
